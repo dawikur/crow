6c13316090bd57dd767d913deb5b1d3e35c01e36
Dawid Kurek <dawikur@gmail.com>
2016-12-12 19:07:27 +0100

    [report/pointer] Add two buttons to report

--- firmware/report_pointer.hpp
+++ firmware/report_pointer.hpp
@@ -10,7 +10,15 @@ namespace Reports {
 
 union PointerRaw {
   PointerRaw() : _{0} {}
-  
+
+  struct Button {
+    static constexpr int8_t Left    = 0x1 << 0;
+    static constexpr int8_t Right   = 0x1 << 1;
+    static constexpr int8_t Middle  = 0x1 << 2;
+    static constexpr int8_t Back    = 0x1 << 3;
+    static constexpr int8_t Forward = 0x1 << 4;
+  };
+
   struct {
     int8_t buttons;
     int8_t x;
@@ -73,9 +81,9 @@ class Pointer : public Base<1, PointerRaw> {
       case '+' ^ 'y': raw.y = Speed; break;
       case '-' ^ 'y': raw.y = -Speed; break;
 
-      case 'B' ^ 'L': raw.buttons |= 0x01; break;
-      case 'B' ^ 'M': raw.buttons |= 0x04; break;
-      case 'B' ^ 'R': raw.buttons |= 0x02; break;
+      case 'B' ^ 'L': raw.buttons |= PointerRaw::Button::Left; break;
+      case 'B' ^ 'R': raw.buttons |= PointerRaw::Button::Right; break;
+      case 'B' ^ 'M': raw.buttons |= PointerRaw::Button::Middle; break;
     }
   }
 
@@ -98,40 +106,41 @@ class Pointer : public Base<1, PointerRaw> {
           raw.y = 0x00;
         break;
 
-      case 'B' ^ 'L': raw.buttons &= ~0x01; break;
-      case 'B' ^ 'M': raw.buttons &= ~0x04; break;
-      case 'B' ^ 'R': raw.buttons &= ~0x02; break;
+      case 'B' ^ 'L': raw.buttons &= ~PointerRaw::Button::Left; break;
+      case 'B' ^ 'R': raw.buttons &= ~PointerRaw::Button::Right; break;
+      case 'B' ^ 'M': raw.buttons &= ~PointerRaw::Button::Middle; break;
     }
   }
 };
 
 static uint8_t const PointerDescriptor[] PROGMEM = {
-  0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-  0x09, 0x02,                    // USAGE (Mouse)
-  0xa1, 0x01,                    // COLLECTION (Application)
+  0x05, 0x01,                    // USAGE_PAGE (GENERIC DESKTOP)
+  0x09, 0x02,                    // USAGE (MOUSE)
+  0xa1, 0x01,                    // COLLECTION (APPLICATION)
   0X85, Pointer::id(),           //   REPORT_ID (1)
-  0x05, 0x09,                    //   USAGE_PAGE (Button)
-  0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-  0x29, 0x03,                    //   USAGE_MAXIMUM (Button 3)
+
+  0x05, 0x09,                    //   USAGE_PAGE (BUTTON)
+  0x19, 0x01,                    //   USAGE_MINIMUM (BUTTON 1)
+  0x29, 0x05,                    //   USAGE_MAXIMUM (BUTTON 5)
   0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
   0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-  0x95, 0x03,                    //   REPORT_COUNT (3)
+  0x95, 0x05,                    //   REPORT_COUNT (5)
   0x75, 0x01,                    //   REPORT_SIZE (1)
-  0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+  0x81, 0x02,                    //   INPUT (DATA,VAR,ABS)
 
   0x95, 0x01,                    //   REPORT_COUNT (1)
-  0x75, 0x05,                    //   REPORT_SIZE (5)
-  0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
+  0x75, 0x03,                    //   REPORT_SIZE (3)
+  0x81, 0x03,                    //   INPUT (CNST,VAR,ABS)
 
-  0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+  0x05, 0x01,                    //   USAGE_PAGE (GENERIC DESKTOP)
   0x09, 0x30,                    //   USAGE (X)
   0x09, 0x31,                    //   USAGE (Y)
-  0x09, 0x38,                    //   USAGE (Wheel)
+  0x09, 0x38,                    //   USAGE (WHEEL)
   0x15, 0x81,                    //   LOGICAL_MINIMUM (-127)
   0x25, 0x7f,                    //   LOGICAL_MAXIMUM (127)
   0x75, 0x08,                    //   REPORT_SIZE (8)
   0x95, 0x03,                    //   REPORT_COUNT (3)
-  0x81, 0x06,                    //   INPUT (Data,Var,Rel)
+  0x81, 0x06,                    //   INPUT (DATA,VAR,REL)
 
   0xc0,                          // END_COLLECTION
 };
